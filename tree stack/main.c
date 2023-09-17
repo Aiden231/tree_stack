0bb1cc2789b96f39f0dc164a9bd4bc3359d1c3c5
Aiden231 <swl0511@naver.com>
2023-09-17 19:41:47 +0900

    과제

--- tree stack/main.c
+++ tree stack/main.c
@@ -8,6 +8,11 @@ typedef struct TreeNode {
 	struct TreeNode* left, * right;
 }TreeNode;
 
+// 스택 생성
+#define SIZE 100
+int top = -1;
+TreeNode* stack[SIZE];
+
 // 이진 트리 LInked로 구현
 TreeNode n11 = { 11,NULL,NULL };
 TreeNode n10 = { 10,NULL,NULL };
@@ -23,11 +28,6 @@ TreeNode n1 = { 1,&n2,&n7 };
 
 // 루트는 n1 이다.
 TreeNode* root = &n1;
-
-// 스택 생성
-#define SIZE 100
-int top = -1;
-TreeNode* stack[SIZE];
 
 // 삽입 함수
 void push(TreeNode* p)
@@ -74,10 +74,35 @@ void inorder_iter(TreeNode* root)
 	}
 }
 
+// 출력 스택
+int top_output = -1;
+TreeNode* stack_output[SIZE];
+
 // 후위 순회 출력 함수
 void postorder_iter(TreeNode* root)
 {
+	if (root == NULL) // 비어있으면 리턴
+		return;
 
+	push(root);
+
+	while (top != -1) {
+		TreeNode* current = pop();
+
+		// 루트를 stack_output에 추가
+		stack_output[++top_output] = current;
+
+		if (current->left)
+			push(current->left);
+		if (current->right)
+			push(current->right);
+	}
+
+	// stack_output을 출력하면 후위 순회 결과가 나옴
+	while (top_output != -1) {
+		TreeNode* current = stack_output[top_output--];
+		printf("%d ", current->data);
+	}
 }
 
 int main()
